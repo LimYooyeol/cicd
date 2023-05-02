42d3e629a9f9c1f8782cfcdcd8bf62e8ec7bccc6
LimYooyeol <duf7040@naver.com>
2023-05-02 14:38:40 +0900

    sh 수정(공백 제거)

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@ REPOSITORY=/home/ubuntu/app
 cd $REPOSITORY
 mkdir shelltest
 
-JAR_NAME = $(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH = $REPOSITORY/build/libs/$JAR_NAME
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID = $(pgrep java)
+CURRENT_PID=$(pgrep java)
 
 if [ -z $CURRENT_PID ]
 then
